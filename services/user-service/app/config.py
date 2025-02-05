3992bd339047a795174fa030b3c73629d3fae022
minhaj-mhd <minhajmuhamd@gmail.com>
2025-02-05 20:52:20 +0530

    shifted to env variable

--- services/user-service/app/config.py
+++ services/user-service/app/config.py
@@ -1,8 +1,9 @@
 from pydantic import BaseSettings
-
+from dotenv import load_dotenv
+import os
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://mj:mjpass@host.docker.internal:5432/mva_user_service"
-    SECRET_KEY: str = "63d6f576c27dda398024310eb8dd10f9350530c97dbde1568b4976dbc1f6da64"
+    DATABASE_URL: str = f"postgresql://{os.getenv("DATABASE_USER")}:{os.getenv("DATABASE_PASSWORD")}@host.docker.internal:5432/mva_user_service"
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES : int = 30
     KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
